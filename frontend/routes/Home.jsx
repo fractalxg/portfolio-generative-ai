ababf4e91c4e13ed90c615b04afa050152f80af7
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2024-03-04 14:16:31 -0300

    feat: initial home structure

---
+++ frontend/routes/Home.jsx
@@ -0,0 +1,16 @@
+import Body from "../components/Body"
+import Footer from "../components/Footer"
+import Header from "../components/Header"
+import "./Home.css"
+
+const Home = () => {
+  return (
+    <div>
+        <Header />
+        <Body />
+        <Footer />
+    </div>
+  )
+}
+
+export default Home
