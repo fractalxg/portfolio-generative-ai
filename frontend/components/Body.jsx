fe269e02c20e716c581c7e811379c1d026adf6fb
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2024-03-13 19:33:58 -0300

    reafactor: drag and drop text logic

--- frontend/components/Body.jsx
+++ frontend/components/Body.jsx
@@ -1,13 +1,14 @@
-import "./Body.css"
-import DragAndDrop from "./DragAndDrop"
+import "./Body.css";
+import DragAndDrop from "./DragAndDrop";
+import { useState } from "react";
 
 const Body = () => {
+  const [dragAndDropText, setDragAndDropText] = useState("");
   return (
     <div className="body-container">
-        <DragAndDrop />
-        
+      <DragAndDrop setDragAndDropText={setDragAndDropText} />
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
