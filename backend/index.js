2e1eebc9b90522ccead0363a44e16ac7092cf56e
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2024-03-18 19:42:35 -0300

    refactor: initial back end structure

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,13 @@ const cors = require("cors");
 const server = express();
 require('dotenv').config()
 
+server.use(
+    cors({
+      origin: process.env.LOCAL_HOST,
+      methods: ["GET", "POST"],
+    })
+  );
+
 server.listen(process.env.PORT, () => {
     console.log(`Server is running on port: ${process.env.PORT}`);
   });
