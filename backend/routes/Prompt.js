2178fd843fa4c9fed90220e61f421c5d6bd616a3
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2024-03-20 18:45:53 -0300

    feat: back end open ai prompt api

--- backend/routes/Prompt.js
+++ backend/routes/Prompt.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const axios = require("axios");
 const router = express.Router();
+const OpenAI = require("openai");
+router.use(express.json());
 
-router.post("/prompt", async (req,res) => {
-   
-})
+const openai = new OpenAI({
+  apiKey: process.env.OPEN_AI_API_KEY,
+});
+
+router.get("/prompt", async (req, res) => {
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo-instruct",
+      message: [{"role":"user","content":"Say this is a test."}],
+      max_tokens: 10,
+      temperature: 0,
+    });
+  res.send(response);
+});
 
 module.exports = router;
