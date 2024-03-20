2f437fc9e90d01ad6d61e7392ac6e242a53261bc
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2024-03-20 17:35:11 -0300

    feat: initial back end prompt route

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const server = express();
-require('dotenv').config()
+require("dotenv").config();
 
 server.use(
-    cors({
-      origin: process.env.LOCAL_HOST,
-      methods: ["GET", "POST"],
-    })
-  );
+  cors({
+    origin: process.env.LOCAL_HOST,
+    methods: ["GET", "POST"],
+  })
+);
+
+const promptRoute = require("./routes/Prompt");
+server.use("/", promptRoute);
 
 server.listen(process.env.PORT, () => {
-    console.log(`Server is running on port: ${process.env.PORT}`);
-  });
+  console.log(`Server is running on port: ${process.env.PORT}`);
+});
---
+++ backend/routes/Prompt.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const axios = require("axios");
+const router = express.Router();
+
+router.post("/prompt", async (req,res) => {
+   
+})
+
+module.exports = router;
