d0bce3fcc6ffc3faf0ca857b2077d96622d591bb
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2024-03-27 23:13:58 -0300

    feat: handle text area input

--- frontend/components/Body.jsx
+++ frontend/components/Body.jsx
@@ -2,10 +2,24 @@ import "./Body.css";
 import DragAndDrop from "./DragAndDrop";
 import { IoMdSend } from "react-icons/io";
 import { useState } from "react";
+import axios from "axios";
 
 const Body = () => {
   const [dragAndDropText, setDragAndDropText] = useState("");
+  const [promptResponse, setPromptResponse] = useState("");
+  const [promptData, setPromptData] = useState("");
+  const [promptValue, setPromptValue] = useState("");
 
+  const handleTextAreaInput = async () => {
+    const getPromptResponse = await axios.post(
+      import.meta.env.VITE_PROMPT_API,
+      {
+        promptData,
+        promptValue,
+      }
+    );
+    setPromptResponse(getPromptResponse);
+  };
 
   return (
     <div className="body-container">
@@ -20,7 +34,9 @@ const Body = () => {
           <h2>Type your prompt</h2>
           <div className="body-text-area-container">
             <textarea></textarea>
-            <button className="send-prompt"><IoMdSend /></button>
+            <button className="send-prompt">
+              <IoMdSend />
+            </button>
           </div>
         </div>
       ) : null}
