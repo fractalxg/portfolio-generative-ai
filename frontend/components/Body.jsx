3d6c68e5fd0e53c5652f443c81a738976d2fb2bc
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2024-04-03 18:07:59 -0300

    feat: generated answear

--- frontend/components/Body.jsx
+++ frontend/components/Body.jsx
@@ -11,16 +11,21 @@ const Body = () => {
   const [promptValue, setPromptValue] = useState("");
   const [dragAndDropVisibility, setDragAndDropVisibility] = useState(true);
   const [hideShowText, setHideShowText] = useState("Hide Text");
+  const [generatedAnswearVisibility, setGeneratedAnswearVisibility] =
+    useState(false);
 
   const handlePrompt = async () => {
-    const getPromptResponse = await axios.post(
-      import.meta.env.VITE_PROMPT_API,
-      {
-        promptData: dragAndDropText,
-        promptValue: promptValue,
-      }
-    );
-    setPromptResponse(getPromptResponse);
+    if (promptValue.length > 0) {
+      const getPromptResponse = await axios.post(
+        import.meta.env.VITE_PROMPT_API,
+        {
+          promptData: dragAndDropText,
+          promptValue: promptValue,
+        }
+      );
+      setPromptResponse(getPromptResponse);
+      setGeneratedAnswearVisibility(true);
+    }
   };
 
   const handleTextAreaInput = (value) => {
@@ -30,6 +35,7 @@ const Body = () => {
   const handleReloadFile = () => {
     setDragAndDropVisibility(true);
     setDragAndDropText("");
+    setGeneratedAnswearVisibility(false);
   };
 
   const handleHideShowText = () => {
@@ -52,7 +58,6 @@ const Body = () => {
           <h2>Received Text</h2>
           <div className="body-text-drag-and-drop-container">
             {hideShowText === "Hide Text" ? <p>{dragAndDropText}</p> : null}
-
             <button
               className="hide-show-text"
               onClick={() => handleHideShowText()}
@@ -75,6 +80,15 @@ const Body = () => {
               <IoMdSend />
             </button>
           </div>
+
+          {generatedAnswearVisibility ? (
+            <div className="body-generated-answear-container">
+              <h2>Generated answear</h2>
+              <div className="body-generated-answear-content">
+                <p>{promptResponse}</p>
+              </div>
+            </div>
+          ) : null}
         </div>
       ) : null}
     </div>
