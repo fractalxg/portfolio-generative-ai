2a5bc7b2194460e593788a1ad15fa3fbac38b370
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2024-04-10 20:55:40 -0300

    feat: inital pre prompt configuration

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,12 +1,31 @@
 import "./Header.css";
 import { FaGear } from "react-icons/fa6";
-import {Rotate} from "./utils/Animation"
+import { Rotate } from "./utils/Animation";
+import { useState } from "react";
 
 const Header = ({ configVisibility }) => {
+  const [promptConfigurationVisibility, setPromptConfigurationVisibility] =
+    useState(false);
+
+  const handlePromptConfigurationVisibility = () => {
+    setPromptConfigurationVisibility(!promptConfigurationVisibility);
+  };
+
   return (
     <div className="header-container">
       <div className="header-content">
-        {configVisibility ? <Rotate><FaGear className="header-icon" /> </Rotate>: null}
+        {configVisibility ? (
+          <Rotate>
+            <FaGear className="header-icon" onClick={() => handlePromptConfigurationVisibility()}/>{" "}
+          </Rotate>
+        ) : null}
+        {promptConfigurationVisibility ? (
+          <div className="header-content-prompt-configuration">
+            <div className="header-content-prompt-configuration-content">
+              
+            </div>
+          </div>
+        ) : null}
       </div>
     </div>
   );
